b7693b7fcd3ca6189bdde41de45883e0d7d09c72
Karthikeyan Jaikumar <karthikeyan.jaikumar@gmail.com>
2022-07-12 20:34:41 -0400

    Student1 marks and total

--- file1.py
+++ file1.py
@@ -1,6 +1,6 @@
 
-Std1_M = [E,T,M,S,SS] = [86, 78, 98, 75, 78]
+Std1_M = [E,T,M,S,SS] = [96, 78, 98, 75, 78]
 Std1_T = 0
 for i in Std1_M:
     Std1_T = Std1_T + i
-print(Std1_T)
+print(Std1_M, Std1_T)
